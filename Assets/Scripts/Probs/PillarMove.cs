e0e5e1bc073e9ab78fb76e4ac4d434f97298a2a1
Chris <c.buchberger01@gmail.com>
2021-05-05 08:28:29 -0700

    merge

--- Assets/Scripts/Probs/PillarMove.cs
+++ Assets/Scripts/Probs/PillarMove.cs
@@ -7,7 +7,7 @@ public class PillarMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.
+        
     }
 
     // Update is called once per frame
