4b5d97976f6234e1a7db315fec5149d2109f662a
Chris <c.buchberger01@gmail.com>
2021-05-06 13:52:50 -0700

    Visual Dart Amount + Mqtt

--- Assets/Scripts/Player/SpawnArrow.cs
+++ Assets/Scripts/Player/SpawnArrow.cs
@@ -8,24 +8,58 @@ public class SpawnArrow : MonoBehaviour
 
     // Script should be attached to spawn point of foam rather than foam itself!!
     public GameObject Arrow;
+    public GameObject FakeArrow;
     public GameObject Player;
+    public GameObject DartPos;
+    private List<GameObject> darts = new List<GameObject>();
     public float speed = 100f;
+    int dartAmount = 30;
 
     bool canShoot = true;
+
+    private void Start()
+    {
+
+        Vector3 pos = DartPos.transform.position;
+        Vector3 rot = new Vector3(0, 90, 0);
+        for(int i = 0; i < dartAmount; i++)
+        {
+            pos.z += 0.1f;
+            GameObject go = Instantiate(FakeArrow);
+            
+            go.transform.position = pos;
+            go.transform.localEulerAngles = rot;
+            darts.Add(go);
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
 
-            if (canShoot)
+            if (canShoot && dartAmount>0)
             {
+                RemoveDart();
                 GameObject shot = GameObject.Instantiate(Arrow, transform.position, transform.rotation);
                 shot.GetComponent<Rigidbody>().AddForce(transform.forward * (-1) * speed);
                 canShoot = false;
                 gameObject.transform.localScale = new Vector3(0, 0, 0);
                 LeanTween.delayedCall(1,SetShoot);
             }
+        }
+    }
+
+    private void RemoveDart()
+    {
+        
+        dartAmount--;
+        Destroy(darts[0]);
+        darts.RemoveAt(0);
+        Mqtt.MqttCurrentDarts(dartAmount);
+        if (dartAmount < 1)
+        {
+            Debug.Log("No Darts Left");
         }
     }
 
